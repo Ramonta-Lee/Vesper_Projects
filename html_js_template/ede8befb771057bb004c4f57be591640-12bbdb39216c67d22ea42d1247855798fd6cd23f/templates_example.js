a4860658d4349eda4c46c95373fbb18954203d2a
Ramonta-Lee <56370633+Ramonta-Lee@users.noreply.github.com>
2024-10-20 17:36:03 -0400

    deleted unnecessary files

--- html_js_template/ede8befb771057bb004c4f57be591640-12bbdb39216c67d22ea42d1247855798fd6cd23f/templates_example.js
+++
@@ -1,8 +0,0 @@
-// #my-snippet
-
-(async () => {
-  const template = await templates.load('my-template.html')
-  console.assert('my-template.html' in templates)
-  console.assert(template === document.head.lastElementChild)
-  console.assert(template instanceof HTMLTemplateElement)
-})()
